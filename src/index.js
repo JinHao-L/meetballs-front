b69a29b254ffec9dc0643e396b1c9ef88a6fb515
Ong Ying Gao <ong.yinggao@gmail.com>
2021-10-08 21:56:59 +0800

    Integrate axios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 ReactDOM.render(
 	<React.StrictMode>
