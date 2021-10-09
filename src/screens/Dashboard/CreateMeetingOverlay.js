23bed0a997819953af5f6fa06ad3cbed52da89d6
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2021-10-09 16:13:54 +0800

    Fix integration and add date/time picker

--- src/screens/Dashboard/CreateMeetingOverlay.js
+++ src/screens/Dashboard/CreateMeetingOverlay.js
@@ -1,79 +1,83 @@
-import { useRef, useState } from "react";
-import { Offcanvas, Form, Button } from "react-bootstrap";
-import { DateTime } from "react-datetime-bootstrap";
-import { accessTokenKey, apiUrl } from "../../common/CommonValues";
+import { useRef, useState } from 'react';
+import { Offcanvas, Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import { accessTokenKey, apiUrl } from '../../common/CommonValues';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function CreateMeetingOverlay({ show, setShow }) {
-	const nameRef = useRef();
-	const descriptionRef = useRef();
-	const hostLinkRef = useRef();
-	const linkRef = useRef();
-	const [startDate, setStartDate] = useState(new Date());
+  const nameRef = useRef();
+  const descriptionRef = useRef();
+  const hostLinkRef = useRef();
+  const linkRef = useRef();
+  const [startDate, setStartDate] = useState(new Date());
+  const history = useHistory();
 
-	async function createMeeting() {
-		//Upload and go to upcoming meeting screen
-		const url = apiUrl + "/meeting";
-		const accessToken = window.sessionStorage.getItem(accessTokenKey);
-		const response = await fetch(url, {
-			method: "POST",
-			headers: {
-				Authorization: "Bearer " + accessToken,
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				name: nameRef.current.value,
-				description: descriptionRef.current.value,
-				duration: 30000,
-				meetingId: "",
-				startUrl: hostLinkRef.current.value,
-				joinUrl: linkRef.current.value,
-				enableTranscription: true,
-				participants: [],
-				agendaItems: [],
-			}),
-		});
-		const error = await response.text();
-		console.log(error);
-		if (response.status === 200) {
-			const result = await response.json();
-			console.log(result);
-		}
-	}
+  async function createMeeting() {
+    //Upload and go to upcoming meeting screen
+    const url = apiUrl + '/meeting';
+    const accessToken = window.sessionStorage.getItem(accessTokenKey);
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + accessToken,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: nameRef.current.value,
+        description: descriptionRef.current.value,
+        startedAt: startDate,
+        duration: 30000,
+        meetingId: '',
+        startUrl: hostLinkRef.current.value,
+        joinUrl: linkRef.current.value,
+        enableTranscription: true,
+      }),
+    });
+    if (response.status === 201) {
+      const result = await response.json();
+      history.push('/meeting/' + result.id);
+      setShow(false);
+    }
+  }
 
-	return (
-		<Offcanvas show={show} onHide={setShow}>
-			<Offcanvas.Header closeButton>
-				<Offcanvas.Title>Create Meeting</Offcanvas.Title>
-			</Offcanvas.Header>
-			<Offcanvas.Body>
-				<Form.Group>
-					<Form.Label column>Meeting Name</Form.Label>
-					<Form.Control ref={nameRef} />
-					<Form.Label column>Host Meeting Link</Form.Label>
-					<Form.Control ref={hostLinkRef} />
-					<Form.Label column>Participant Meeting Link</Form.Label>
-					<Form.Control ref={linkRef} />
-					<Form.Label column>Start Date {"&"} Time</Form.Label>
-					<DateTime
-						value={startDate.toISOString()}
-						onChange={(value) => setStartDate(new Date(value))}
-					/>
-
-					<Form.Label column>Description</Form.Label>
-					<Form.Control
-						as="textarea"
-						style={{ height: 200 }}
-						ref={descriptionRef}
-					/>
-				</Form.Group>
-				<div className="Buffer--20px" />
-				<div className="d-grid gap-2">
-					<Button variant="secondary" onClick={createMeeting}>
-						Create
-					</Button>
-				</div>
-			</Offcanvas.Body>
-		</Offcanvas>
-	);
+  return (
+    <Offcanvas show={show} onHide={setShow}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Create Meeting</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Form.Group>
+          <Form.Label column>Meeting Name</Form.Label>
+          <Form.Control ref={nameRef} />
+          <Form.Label column>Host Meeting Link</Form.Label>
+          <Form.Control ref={hostLinkRef} />
+          <Form.Label column>Participant Meeting Link</Form.Label>
+          <Form.Control ref={linkRef} />
+          <Form.Label column>Start Date {'&'} Time</Form.Label>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            timeInputLabel="Time:"
+            dateFormat="MM/dd/yyyy h:mm aa"
+            showTimeInput
+            customInput={<Form.Control />}
+          />
+          <Form.Label column>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            style={{ height: 200 }}
+            ref={descriptionRef}
+          />
+        </Form.Group>
+        <div className="Buffer--20px" />
+        <div className="d-grid gap-2">
+          <Button variant="secondary" onClick={createMeeting}>
+            Create
+          </Button>
+        </div>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
 }
--- src/screens/UpcomingMeeting/UpcomingMeetingScreen.js
+++ src/screens/UpcomingMeeting/UpcomingMeetingScreen.js
@@ -1,230 +1,213 @@
-import { useState, useEffect } from "react";
-import { Button, Row, Col, Container, Nav, Alert } from "react-bootstrap";
-import { getFormattedDateTime } from "../../common/CommonFunctions";
-import AgendaItemList from "./AgendaItemList";
-import ParticipantItemList from "./ParticipantItemList";
-import { PersonPlusFill, CalendarPlusFill } from "react-bootstrap-icons";
+import { useState, useEffect } from 'react';
+import { Button, Row, Col, Container, Nav, Alert } from 'react-bootstrap';
+import { getFormattedDateTime } from '../../common/CommonFunctions';
+import AgendaItemList from './AgendaItemList';
+import ParticipantItemList from './ParticipantItemList';
+import { PersonPlusFill, CalendarPlusFill } from 'react-bootstrap-icons';
 import {
-	blankAgenda,
-	blankMeeting,
-	blankParticipant,
-} from "../../common/ObjectTemplates";
-import { accessTokenKey, apiUrl } from "../../common/CommonValues";
-import EditMeetingOverlay from "./EditMeetingOverlay";
-import { useHistory, Redirect, useParams } from "react-router";
+  blankAgenda,
+  blankMeeting,
+  blankParticipant,
+} from '../../common/ObjectTemplates';
+import { accessTokenKey, apiUrl } from '../../common/CommonValues';
+import EditMeetingOverlay from './EditMeetingOverlay';
+import { useHistory, Redirect, useParams } from 'react-router';
 
 export default function UpcomingMeetingScreen() {
-	const [meeting, setMeeting] = useState(blankMeeting);
-	const [restrictDescription, setRestrictDescription] = useState(true);
-	const [currentTab, setCurrentTab] = useState("agenda");
-	const [showEditMeeting, setShowEditMeeting] = useState(false);
-	const [isReordering, setReordering] = useState(false);
-	const history = useHistory();
+  const [meeting, setMeeting] = useState(blankMeeting);
+  const [restrictDescription, setRestrictDescription] = useState(true);
+  const [currentTab, setCurrentTab] = useState('agenda');
+  const [showEditMeeting, setShowEditMeeting] = useState(false);
+  const [isReordering, setReordering] = useState(false);
+  const history = useHistory();
 
-	const { id } = useParams();
+  const { id } = useParams();
 
-	useEffect(() => {
-		pullMeeting();
-	}, []);
+  useEffect(() => {
+    pullMeeting();
+  }, []);
 
-	async function pullMeeting() {
-		const url = apiUrl + "/meeting/" + id;
-		const response = await fetch(url, {
-			method: "GET",
-		});
-		const result = await response.json();
-		result.agendaItems.sort((p1, p2) => {
-			return p1.position - p2.position;
-		});
-		result.agendaItems.forEach((item) => {
-			item.prevPosition = item.position;
-		});
-		console.log(result);
-		setMeeting(result);
-	}
+  async function pullMeeting() {
+    const url = apiUrl + '/meeting/' + id;
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+    const result = await response.json();
+    result.agendaItems.sort((p1, p2) => {
+      return p1.position - p2.position;
+    });
+    result.agendaItems.forEach((item) => {
+      item.prevPosition = item.position;
+    });
+    console.log(result);
+    setMeeting(result);
+  }
 
-	function startZoom() {
-		meeting.type = 2;
-		window.open(meeting.startUrl, "_blank");
-		history.replace("/ongoing/" + id);
-	}
+  function startZoom() {
+    meeting.type = 2;
+    window.open(meeting.startUrl, '_blank');
+    history.replace('/ongoing/' + id);
+  }
 
-	function Content() {
-		if (currentTab === "agenda") {
-			return (
-				<AgendaItemList
-					meeting={meeting}
-					setMeeting={setMeeting}
-					isReordering={isReordering}
-					setReordering={setReordering}
-				/>
-			);
-		} else {
-			return (
-				<ParticipantItemList
-					meeting={meeting}
-					setMeeting={setMeeting}
-				/>
-			);
-		}
-	}
+  function Content() {
+    if (currentTab === 'agenda') {
+      return (
+        <AgendaItemList
+          meeting={meeting}
+          setMeeting={setMeeting}
+          isReordering={isReordering}
+          setReordering={setReordering}
+        />
+      );
+    } else {
+      return <ParticipantItemList meeting={meeting} setMeeting={setMeeting} />;
+    }
+  }
 
-	function AddToggle() {
-		if (currentTab === "participants") {
-			return (
-				<div
-					className="Fab"
-					onClick={() => addParticipant(meeting, setMeeting)}
-				>
-					<PersonPlusFill size={25} color="white" />
-				</div>
-			);
-		}
-		return (
-			<div className="Fab" onClick={() => addAgenda(meeting, setMeeting)}>
-				<CalendarPlusFill size={22} color="white" />
-			</div>
-		);
-	}
+  function AddToggle() {
+    if (currentTab === 'participants') {
+      return (
+        <div
+          className="Fab"
+          onClick={() => addParticipant(meeting, setMeeting)}
+        >
+          <PersonPlusFill size={25} color="white" />
+        </div>
+      );
+    }
+    return (
+      <div className="Fab" onClick={() => addAgenda(meeting, setMeeting)}>
+        <CalendarPlusFill size={22} color="white" />
+      </div>
+    );
+  }
 
-	if (meeting.type !== 1) {
-		return <Redirect to={"/ongoing/" + id} />;
-	}
+  if (meeting.type !== 1) {
+    return <Redirect to={'/ongoing/' + id} />;
+  }
 
-	return (
-		<>
-			<Container className="Container__padding--vertical">
-				<Row>
-					<Col
-						lg={3}
-						md={12}
-						sm={12}
-						className="Container__padding--horizontal"
-					>
-						<p className="Text__header">{meeting.name}</p>
-						<p className="Text__subheader">
-							{getFormattedDateTime(meeting.startedAt)}
-						</p>
-						<div className="d-grid gap-2">
-							<Button onClick={startZoom}>
-								Start Zoom Meeting
-							</Button>
-							<Button variant="outline-primary">
-								Email Participants
-							</Button>
-							<Button
-								variant="outline-secondary"
-								onClick={() => setShowEditMeeting(true)}
-							>
-								Edit / Delete Meeting
-							</Button>
-						</div>
-						<p className="Text__subsubheader">Description</p>
-						<p
-							className={
-								"Text__paragraph" +
-								(restrictDescription
-									? " Text__elipsized--5-lines"
-									: "")
-							}
-						>
-							{meeting.description}
-						</p>
-						<div className="d-grid gap-2">
-							<Button
-								variant="secondary"
-								onClick={() =>
-									setRestrictDescription(!restrictDescription)
-								}
-							>
-								View {restrictDescription ? "More" : "Less"}
-							</Button>
-						</div>
-						<div className="Buffer--20px" />
-					</Col>
-					<Col lg={1} md={12} sm={12} />
-					<Col
-						lg={6}
-						md={12}
-						sm={12}
-						className="Container__padding--horizontal"
-					>
-						<Nav
-							variant="tabs"
-							defaultActiveKey="agenda"
-							onSelect={(selectedKey) =>
-								setCurrentTab(selectedKey)
-							}
-						>
-							<Nav.Item>
-								<Nav.Link eventKey="agenda">Agenda</Nav.Link>
-							</Nav.Item>
-							<Nav.Item>
-								<Nav.Link eventKey="participants">
-									Participants
-								</Nav.Link>
-							</Nav.Item>
-						</Nav>
-						<div className="Buffer--20px" />
-						<Content />
-						<div className="Buffer--100px" />
-					</Col>
-				</Row>
-			</Container>
-			<EditMeetingOverlay
-				show={showEditMeeting}
-				setShow={setShowEditMeeting}
-				meeting={meeting}
-				setMeeting={setMeeting}
-			/>
-			<AddToggle />
-		</>
-	);
+  return (
+    <>
+      <Container className="Container__padding--vertical">
+        <Row>
+          <Col
+            lg={3}
+            md={12}
+            sm={12}
+            className="Container__padding--horizontal"
+          >
+            <p className="Text__header">{meeting.name}</p>
+            <p className="Text__subheader">
+              {getFormattedDateTime(meeting.startedAt)}
+            </p>
+            <div className="d-grid gap-2">
+              <Button onClick={startZoom}>Start Zoom Meeting</Button>
+              <Button variant="outline-primary">Email Participants</Button>
+              <Button
+                variant="outline-secondary"
+                onClick={() => setShowEditMeeting(true)}
+              >
+                Edit / Delete Meeting
+              </Button>
+            </div>
+            <p className="Text__subsubheader">Description</p>
+            <p
+              className={
+                'Text__paragraph' +
+                (restrictDescription ? ' Text__elipsized--5-lines' : '')
+              }
+            >
+              {meeting.description}
+            </p>
+            <div className="d-grid gap-2">
+              <Button
+                variant="secondary"
+                onClick={() => setRestrictDescription(!restrictDescription)}
+              >
+                View {restrictDescription ? 'More' : 'Less'}
+              </Button>
+            </div>
+            <div className="Buffer--20px" />
+          </Col>
+          <Col lg={1} md={12} sm={12} />
+          <Col
+            lg={6}
+            md={12}
+            sm={12}
+            className="Container__padding--horizontal"
+          >
+            <Nav
+              variant="tabs"
+              defaultActiveKey="agenda"
+              onSelect={(selectedKey) => setCurrentTab(selectedKey)}
+            >
+              <Nav.Item>
+                <Nav.Link eventKey="agenda">Agenda</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="participants">Participants</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <div className="Buffer--20px" />
+            <Content />
+            <div className="Buffer--100px" />
+          </Col>
+        </Row>
+      </Container>
+      <EditMeetingOverlay
+        show={showEditMeeting}
+        setShow={setShowEditMeeting}
+        meeting={meeting}
+        setMeeting={setMeeting}
+      />
+      <AddToggle />
+    </>
+  );
 }
 
 function addParticipant(meeting, setMeeting) {
-	if (meeting.participants.findIndex((item) => item.userEmail === "") >= 0)
-		return;
-	const newMeeting = Object.assign({}, meeting);
-	const newParticipant = Object.assign({}, blankParticipant);
-	newParticipant.meetingId = newMeeting.id;
-	newMeeting.participants = [...newMeeting.participants, newParticipant];
-	setMeeting(newMeeting);
+  if (meeting.participants.findIndex((item) => item.userEmail === '') >= 0)
+    return;
+  const newMeeting = Object.assign({}, meeting);
+  const newParticipant = Object.assign({}, blankParticipant);
+  newParticipant.meetingId = newMeeting.id;
+  newMeeting.participants = [...newMeeting.participants, newParticipant];
+  setMeeting(newMeeting);
 }
 
 async function addAgenda(meeting, setMeeting) {
-	const newMeeting = Object.assign({}, meeting);
-	const newAgenda = Object.assign({}, blankAgenda);
-	newAgenda.meetingId = newMeeting.id;
-	const size = newMeeting.agendaItems.length;
-	if (size > 0) {
-		const lastItem = newMeeting.agendaItems[size - 1];
-		newAgenda.position = lastItem.position + 1;
-	} else {
-		newAgenda.position = 0;
-	}
-	newAgenda.prevPosition = newAgenda.position;
-	await addAgendaToDatabase(newAgenda);
-	newMeeting.agendaItems = [...newMeeting.agendaItems, newAgenda];
-	setMeeting(newMeeting);
+  const newMeeting = Object.assign({}, meeting);
+  const newAgenda = Object.assign({}, blankAgenda);
+  newAgenda.meetingId = newMeeting.id;
+  const size = newMeeting.agendaItems.length;
+  if (size > 0) {
+    const lastItem = newMeeting.agendaItems[size - 1];
+    newAgenda.position = lastItem.position + 1;
+  } else {
+    newAgenda.position = 0;
+  }
+  newAgenda.prevPosition = newAgenda.position;
+  await addAgendaToDatabase(newAgenda);
+  newMeeting.agendaItems = [...newMeeting.agendaItems, newAgenda];
+  setMeeting(newMeeting);
 }
 
 async function addAgendaToDatabase(newAgenda) {
-	const url = apiUrl + "/agenda-item";
-	const accessToken = window.sessionStorage.getItem(accessTokenKey);
-	await fetch(url, {
-		method: "POST",
-		headers: {
-			Authorization: "Bearer " + accessToken,
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			meetingId: newAgenda.meetingId,
-			position: newAgenda.position,
-			name: newAgenda.name,
-			description: newAgenda.description,
-			expectedDuration: newAgenda.expectedDuration,
-		}),
-	});
+  const url = apiUrl + '/agenda-item';
+  const accessToken = window.sessionStorage.getItem(accessTokenKey);
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + accessToken,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      meetingId: newAgenda.meetingId,
+      position: newAgenda.position,
+      name: newAgenda.name,
+      description: newAgenda.description,
+      expectedDuration: newAgenda.expectedDuration,
+    }),
+  });
 }
