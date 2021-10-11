5ba601f5556fa69bd035806af3929021442aeeac
Ong Ying Gao <ong.yinggao@gmail.com>
2021-10-11 17:29:00 +0800

    Add horizontal padding to dashboard screen

--- src/screens/Dashboard/DashboardScreen.js
+++ src/screens/Dashboard/DashboardScreen.js
@@ -1,144 +1,151 @@
-import { useEffect, useState } from "react";
-import { Tab, Tabs, Spinner, Container } from "react-bootstrap";
-import { CalendarPlusFill } from "react-bootstrap-icons";
-import CompletedMeetingItem from "./CompletedMeetingItem";
-import UpcomingMeetingItem from "./UpcomingMeetingItem";
-import { defaultHeaders } from "../../utils/axiosConfig";
-import AddMeetingOverlay from "./AddMeetingOverlay";
-import server from "../../services/server";
-import { useHistory } from "react-router";
+import { useEffect, useState } from 'react';
+import { Tab, Tabs, Spinner, Container } from 'react-bootstrap';
+import { CalendarPlusFill } from 'react-bootstrap-icons';
+import CompletedMeetingItem from './CompletedMeetingItem';
+import UpcomingMeetingItem from './UpcomingMeetingItem';
+import { defaultHeaders } from '../../utils/axiosConfig';
+import AddMeetingOverlay from './AddMeetingOverlay';
+import server from '../../services/server';
+import { useHistory } from 'react-router';
 
 function AddMeetingButton({ onClick }) {
-	return (
-		<div className="Fab" onClick={onClick}>
-			<CalendarPlusFill size={22} color="white" />
-		</div>
-	);
+  return (
+    <div className="Fab" onClick={onClick}>
+      <CalendarPlusFill size={22} color="white" />
+    </div>
+  );
 }
 
 export default function DashboardScreen() {
-	const [upcoming, setUpcoming] = useState([]);
-	const [meetingHistory, setHistory] = useState([]);
-	
-	const [loadingUpcoming, setLoadingUpcoming] = useState(true);
-	const [loadingPast, setLoadingPast] = useState(true);
+  const [upcoming, setUpcoming] = useState([]);
+  const [meetingHistory, setHistory] = useState([]);
 
-	const [showOverlay, setShowOverlay] = useState(false);
+  const [loadingUpcoming, setLoadingUpcoming] = useState(true);
+  const [loadingPast, setLoadingPast] = useState(true);
 
-	const history = useHistory();
+  const [showOverlay, setShowOverlay] = useState(false);
 
-	useEffect(() => {
-		return pullMeetings();
-	}, []);
+  const history = useHistory();
 
-	function pullMeetings() {
-		pullPastMeetings();
-		pullUpcomingMeetings();
-	}
+  useEffect(() => {
+    return pullMeetings();
+  }, []);
 
-	function pullUpcomingMeetings() {
-		function sortMeetings(meetingA, meetingB) {
-			const startA = meetingA.startedAt;
-			const startB = meetingB.startedAt;
-			if (startA < startB) return 1;
-			else if (startA > startB) return -1;
-			else return 0;
-		}
-		return server.get('/meeting', { 
-			params: { type: "upcoming" },
-			...defaultHeaders 
-		})
-		.then((res) => {
-			console.log(res);
-			const meetings = res.data;
-			const upcoming = meetings.sort(sortMeetings);
-			setUpcoming(upcoming);
-			setHistory(meetingHistory);
-		})
-		.catch(console.error)
-		.finally(() => setLoadingUpcoming(false));
-	}
+  function pullMeetings() {
+    pullPastMeetings();
+    pullUpcomingMeetings();
+  }
 
-	function pullPastMeetings() {
-		function sortMeetings(meetingA, meetingB) {
-			const startA = meetingA.startedAt;
-			const startB = meetingB.startedAt;
-			if (startA > startB) return 1;
-			else if (startA < startB) return -1;
-			else return 0;
-		}
-		console.log("Retrieving past meetings");
-		return server.get('/meeting', { 
-			params: { type: "past" },
-			...defaultHeaders 
-		})
-		.then((res) => {
-			console.log(res);
-			const pastMeetings = res.data.sort(sortMeetings);
-			setHistory(pastMeetings);
-		})
-		.catch(console.error)
-		.finally(() => setLoadingPast(false));
-	}
-
-	const upcomingList = upcoming.map((meeting, idx) => {
-		return (
-			<UpcomingMeetingItem
-				key={idx}
-				meeting={meeting}
-				editMeeting={() => {
-					const id = meeting.id;
-					console.log("Edit meeting clicked");
-					history.push('/meeting/' + id);
-				}}
-			/>
-		);
-	});
-
-	const historyList = meetingHistory.map((meeting, idx) => {
-		return (
-			<CompletedMeetingItem
-				key={idx}
-				meeting={meeting}
-				viewMeeting={() => {
-					console.log("View meeting report clicked");
-				}}
-			/>
-		);
-	});
-
-	if (loadingUpcoming) {
-        return (
-            <>
-                <Container className="Container__padding--vertical">
-                    <Spinner animation="border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </Spinner>
-                </Container>
-            </>
-        );
+  function pullUpcomingMeetings() {
+    function sortMeetings(meetingA, meetingB) {
+      const startA = meetingA.startedAt;
+      const startB = meetingB.startedAt;
+      if (startA < startB) return 1;
+      else if (startA > startB) return -1;
+      else return 0;
     }
+    return server
+      .get('/meeting', {
+        params: { type: 'upcoming' },
+        ...defaultHeaders,
+      })
+      .then((res) => {
+        console.log(res);
+        const meetings = res.data;
+        const upcoming = meetings.sort(sortMeetings);
+        setUpcoming(upcoming);
+        setHistory(meetingHistory);
+      })
+      .catch(console.error)
+      .finally(() => setLoadingUpcoming(false));
+  }
 
-	return (
-		<>
-			<div className="Container__padding--vertical">
-				<Tabs
-					className="Container__padding--horizontal"
-					defaultActiveKey="upcoming"
-					transition={false}
-					id="meetings-tabs"
-					className="justify-content-center"
-				>
-					<Tab eventKey="upcoming" title="Upcoming Meetings">
-						{upcomingList}
-					</Tab>
-					<Tab eventKey="past" title="Past Meetings">
-						{historyList}
-					</Tab>
-				</Tabs>
-			</div>
-			<AddMeetingOverlay show={showOverlay} setShow={setShowOverlay} onUpdate={pullMeetings} />
-			<AddMeetingButton onClick={() => setShowOverlay(true)} />
-		</>
-	);
+  function pullPastMeetings() {
+    function sortMeetings(meetingA, meetingB) {
+      const startA = meetingA.startedAt;
+      const startB = meetingB.startedAt;
+      if (startA > startB) return 1;
+      else if (startA < startB) return -1;
+      else return 0;
+    }
+    console.log('Retrieving past meetings');
+    return server
+      .get('/meeting', {
+        params: { type: 'past' },
+        ...defaultHeaders,
+      })
+      .then((res) => {
+        console.log(res);
+        const pastMeetings = res.data.sort(sortMeetings);
+        setHistory(pastMeetings);
+      })
+      .catch(console.error)
+      .finally(() => setLoadingPast(false));
+  }
+
+  const upcomingList = upcoming.map((meeting, idx) => {
+    return (
+      <UpcomingMeetingItem
+        key={idx}
+        meeting={meeting}
+        editMeeting={() => {
+          const id = meeting.id;
+          console.log('Edit meeting clicked');
+          history.push('/meeting/' + id);
+        }}
+      />
+    );
+  });
+
+  const historyList = meetingHistory.map((meeting, idx) => {
+    return (
+      <CompletedMeetingItem
+        key={idx}
+        meeting={meeting}
+        viewMeeting={() => {
+          console.log('View meeting report clicked');
+        }}
+      />
+    );
+  });
+
+  if (loadingUpcoming) {
+    return (
+      <>
+        <Container className="Container__padding--vertical">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </Container>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="Container__padding--vertical">
+        <Container className="Container__padding--horizontal">
+          <Tabs
+            defaultActiveKey="upcoming"
+            transition={false}
+            id="meetings-tabs"
+            className="justify-content-center"
+          >
+            <Tab eventKey="upcoming" title="Upcoming Meetings">
+              {upcomingList}
+            </Tab>
+            <Tab eventKey="past" title="Past Meetings">
+              {historyList}
+            </Tab>
+          </Tabs>
+        </Container>
+      </div>
+      <AddMeetingOverlay
+        show={showOverlay}
+        setShow={setShowOverlay}
+        onUpdate={pullMeetings}
+      />
+      <AddMeetingButton onClick={() => setShowOverlay(true)} />
+    </>
+  );
 }
--- src/screens/Login/EmailConfirmationScreen.js
+++ src/screens/Login/EmailConfirmationScreen.js
@@ -1,115 +1,115 @@
-import { useEffect, useState } from "react";
-import { Spinner, Container, Col, Form, Toast, Button } from "react-bootstrap";
-import { useLocation } from "react-router";
-import server from "../../services/server";
+import { useEffect, useState } from 'react';
+import { Spinner, Container, Col, Form, Toast, Button } from 'react-bootstrap';
+import { useLocation } from 'react-router';
+import server from '../../services/server';
 
 function ResendConfirmationForm() {
+  const [email, setEmail] = useState('');
+  function readyToSubmit() {
+    return email.trim().length > 0;
+  }
 
-    const [ email, setEmail ] = useState("");
-    function readyToSubmit() {
-        return email.trim().length > 0;
-    }
+  const [resent, setResent] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-    const [ resent, setResent ] = useState(false);
-    const [ success, setSuccess ] = useState(false);
+  function submit() {
+    return server
+      .post('/auth/resend-confirm', {
+        email: email,
+      })
+      .then((response) => {
+        if (response.data.success) setSuccess(true);
+      })
+      .catch((error) => {
+        console.error(error);
+        setSuccess(false);
+      })
+      .finally(() => setResent(true));
+  }
 
-    function submit() {
-        return server.post('/auth/resend-confirm', {
-            email: email
-        }).then((response) => {
-            if (response.data.success) setSuccess(true);
-        }).catch((error) => {
-            console.error(error);
-            setSuccess(false);
-        }).finally(() => setResent(true));
-    }
-
-    return (
-        <div>
-            <Form onSubmit={submit}>
-                <Form.Group className="mb-3" controlId="formResend">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        type="email"
-                        autoFocus
-                        value={email}
-                        onChange={setEmail}
-                        placeholder="Enter email here"
-                    />
-                </Form.Group>
-                <Button
-                    block
-                    size="me"
-                    disabled={!readyToSubmit()}
-                    type="submit"
-                >
-                    Resend confirmation email
-                </Button>
-            </Form>
-            <Toast show={resent && success}>
-                <Toast.Header>
-                    <strong className="me-auto">Success!</strong>
-                </Toast.Header>
-                <Toast.Body>Confirmation email has been resent to your inbox</Toast.Body>
-            </Toast>
-            <Toast show={resent && !success}>
-                <Toast.Header>
-                    <strong className="me-auto">Error!</strong>
-                </Toast.Header>
-                <Toast.Body>Email not found, are you sure you have registered?</Toast.Body>
-            </Toast>
-        </div>
-    );
+  return (
+    <div>
+      <Form onSubmit={submit}>
+        <Form.Group className="mb-3" controlId="formResend">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            autoFocus
+            value={email}
+            onChange={setEmail}
+            placeholder="Enter email here"
+          />
+        </Form.Group>
+        <Button block size="me" disabled={!readyToSubmit()} type="submit">
+          Resend confirmation email
+        </Button>
+      </Form>
+      <Toast show={resent && success}>
+        <Toast.Header>
+          <strong className="me-auto">Success!</strong>
+        </Toast.Header>
+        <Toast.Body>
+          Confirmation email has been resent to your inbox
+        </Toast.Body>
+      </Toast>
+      <Toast show={resent && !success}>
+        <Toast.Header>
+          <strong className="me-auto">Error!</strong>
+        </Toast.Header>
+        <Toast.Body>
+          Email not found, are you sure you have registered?
+        </Toast.Body>
+      </Toast>
+    </div>
+  );
 }
 
 export default function EmailConfirmationScreen() {
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  const token = query.get('token');
 
-    const { search } = useLocation();
-    const query = new URLSearchParams(search);
-    const token = query.get('token');
+  const [responseMsg, setResponseMsg] = useState('');
+  const [isLoading, setLoading] = useState(true);
+  const [failed, setFailed] = useState(false);
 
-    const [ responseMsg, setResponseMsg ] = useState('');
-    const [ isLoading, setLoading ] = useState(true);
-    const [ failed, setFailed ] = useState(false);
+  useEffect(() => {
+    console.log(`Begin email confirmation! Token is ${token}`);
+    return server
+      .post('/auth/confirm', {
+        token: token,
+      })
+      .then((res) => {
+        console.log('Email confirmation');
+        setResponseMsg(res.data.message);
+      })
+      .catch((e) => {
+        console.error(e);
+        setFailed(true);
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-    useEffect(() => {
-        console.log(`Begin email confirmation! Token is ${token}`);
-        return server.post('/auth/confirm', {
-            token: token
-        })
-        .then(res => {
-            console.log('Email confirmation');
-            setResponseMsg(res.data.message);
-        })
-        .catch(e => {
-            console.error(e);
-            setFailed(true);
-        })
-        .finally(() => setLoading(false));
-    }, []);
-
-    if (isLoading) {
-        return (
-            <>
-                <Container className="Container__padding--vertical">
-                    <Spinner animation="border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </Spinner>
-                </Container>
-            </>
-        );
-    }
-
+  if (isLoading) {
     return (
-        <>
-            <Container className="Container__padding--vertical">
-                <Col className="Container__padding--horizontal" >
-                    <p>
-                        { failed ? "Email confirmation failed" : responseMsg }
-                    </p>
-                    { failed ? <ResendConfirmationForm /> : null }
-                </Col>
-            </Container>
-        </>
+      <>
+        <Container className="Container__padding--vertical">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </Container>
+      </>
     );
+  }
+
+  return (
+    <>
+      <Container className="Container__padding--vertical">
+        <Col className="Container__padding--horizontal">
+          <p>{failed ? 'Email confirmation failed' : responseMsg}</p>
+          {failed ? <ResendConfirmationForm /> : null}
+        </Col>
+      </Container>
+    </>
+  );
 }
