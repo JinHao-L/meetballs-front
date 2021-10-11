e86c1379be85eb5a3f9fa01284038bd6cb3262e0
Ong Ying Gao <ong.yinggao@gmail.com>
2021-10-11 20:57:33 +0800

    store secrets in .env.local

--- src/common/CommonValues.js
+++ src/common/CommonValues.js
@@ -1,3 +1,3 @@
-export const apiUrl = 'https://meetballs.herokuapp.com';
-export const accessTokenKey = 'AccessToken';
-export const refreshTokenKey = 'RefreshToken';
+export const apiUrl = process.env.REACT_APP_API_URL;
+export const accessTokenKey = process.env.REACT_APP_ACCESS_TOKEN_KEY;
+export const refreshTokenKey = process.env.REACT_APP_REFRESH_TOKEN_KEY;
