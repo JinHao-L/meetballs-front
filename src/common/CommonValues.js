73b8c82674d9bd80352a06ca1a07820d09b2ca93
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2021-10-12 16:14:26 +0800

    Remove link

--- src/common/CommonValues.js
+++ src/common/CommonValues.js
@@ -1,4 +1,3 @@
-//export const apiUrl = process.env.REACT_APP_API_URL;
-export const apiUrl = 'https://meetballs.herokuapp.com';
+export const apiUrl = process.env.REACT_APP_API_URL;
 export const accessTokenKey = process.env.REACT_APP_ACCESS_TOKEN_KEY;
 export const refreshTokenKey = process.env.REACT_APP_REFRESH_TOKEN_KEY;
